5581baf850e29b5c7ffb1d04231422fd9c2cd3ed
blacby <1398212034@qq.com>
2024-07-12 01:30:56 +0800

    用户超时强踢功能（bug learn）

--- src/server.go
+++ src/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"strconv"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -35,10 +36,18 @@ func (s *Server) HandleConnection(conn net.Conn) {
 		s,
 	)
 	user.UserOnline()
-
+	//写这个发不出第二次消息了，没有初始化导致发不出来写不进去了
+	//var isAlive chan bool
+	isAlive := make(chan bool)
+	//question1 ：var isAlive chan bool  当为初始化为nil时，
+	//发送一次消息会导致 go协程卡死，
+	//无论发什么都不能延长时间，user map 未能offline，仍旧存在，
+	//当这个user 超时后，有客户端重新链接会导致 广播时用到了关闭了的这个用户的channel
+	//question2：为什么select case 读取nil channel时不会直接卡死呢？
+	//anwer2:在你的 select 语句中，有一个 <-isAlive 的 case，但由于 isAlive 是 nil，这个 case 实际上是一个无效的 case。在 Go 语言中，当 select 语句中的所有 case 都无法执行时，会阻塞在 select 语句处，直到有一个 case 可以执行为止。因此，尽管 <-isAlive 是一个无效的操作（因为 isAlive 是 nil），但它并不会导致整个 handleConnection 协程卡住，因为还有其他的 case（比如 time.After）可以执行。
 	go func() {
+		buf := make([]byte, 4096)
 		for {
-			buf := make([]byte, 4096)
 			n, err := user.con.Read(buf)
 			if n == 0 {
 				user.UserOffline()
@@ -50,10 +59,25 @@ func (s *Server) HandleConnection(conn net.Conn) {
 			}
 			msg := string(buf[:n-1])
 			user.DoMessage(msg)
+			isAlive <- true
 		}
-
 	}()
 
+	//test:
+	//<-isAlive 为什么在这里写这个 handle协程永远卡住，但是瞎买可能的 select不会导致整个handle协程卡住？
+
+	for {
+		select {
+		case <-isAlive:
+
+		case <-time.After(10 * time.Second):
+			user.SendMsg("your client overtime,unactivity so long")
+			//question2： user.UserOffline()
+			close(user.uc)
+			conn.Close()
+			return
+		}
+	}
 }
 
 // BoradCast user online message to every user 触发用户上线消息
